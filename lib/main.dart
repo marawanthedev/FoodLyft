5031dcfa781bb936fb4d1730de5cf7ad56505457
Omar Hamed Ibrahim <Omarhamedchep93@gmail.com>
2021-01-11 03:00:11 +0800

    Pulling From the master branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:foodlyft/screens/FoodMenu/Food_Menu.dart';
+import 'package:foodlyft/screens/FoodMenu/Food_Menu2.dart';
+import 'package:foodlyft/screens/Restaurantlist/Restaurant_List.dart';
+import 'package:foodlyft/screens/cart/cart.dart';
 import 'screens/payment/payment_failed.dart';
 import 'screens/payment/payment_succesfull.dart';
 import 'screens/payment/payment_form.dart';
 import "./screens/Login/login.dart";
 import "./screens/Signup/Signup.dart";
-<<<<<<< HEAD
 // import "./services/router.dart" as router;
 import 'screens/admin/new_restaurant.dart';
 import 'screens/admin/edit-restaurant.dart';
@@ -14,28 +16,16 @@ import 'screens/admin/dialog/delete_dialog.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: "/new_restaurant",
-=======
-import './screens/Restaurantlist/Restaurant_List.dart';
-import "./screens/cart/cart.dart";
-import './screens/FoodMenu/Food_Menu.dart';
-import './screens/FoodMenu/Food_Menu2.dart';
-
-void main() {
-  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: "/Food_Menu2",
->>>>>>> 5d95c19a7fb78ca88bc69ffd4270d38e89ee2e63
     onGenerateRoute: (settings) {
       switch (settings.name) {
         case "/":
         case "/payment_form":
           return MaterialPageRoute(builder: (context) => PaymentForm());
-
         case "/payment_suceeded":
           return MaterialPageRoute(
               builder: (context) => PaymentSuceeded(settings.arguments));
-
         case "/payment_failed":
           return MaterialPageRoute(
               builder: (context) => PaymentFailed(settings.arguments));
@@ -44,7 +34,6 @@ void main() {
               builder: (context) => LoginScreen(settings.arguments));
         case "/signup":
           return MaterialPageRoute(builder: (context) => SignUpScreen());
-<<<<<<< HEAD
         case "/new_restaurant":
           return MaterialPageRoute(builder: (context) => New_Restaurant());
         case "/edit_restaurant":
@@ -53,16 +42,12 @@ void main() {
           return MaterialPageRoute(builder: (context) => Back_Home_Dialog());
         case "/delete_dialog":
           return MaterialPageRoute(builder: (context) => Delete_Dialog());
-=======
+        case "/Food_Menu2":
+          return MaterialPageRoute(builder: (context) => FoodMenu2());
         case "/Restaurant_List":
           return MaterialPageRoute(builder: (context) => RestaurantMenu());
         case "/cart":
           return MaterialPageRoute(builder: (context) => CartScreen());
-        case "/Food_Menu":
-          return MaterialPageRoute(builder: (context) => FoodMenu());
-        case "/Food_Menu2":
-          return MaterialPageRoute(builder: (context) => FoodMenu2());
->>>>>>> 5d95c19a7fb78ca88bc69ffd4270d38e89ee2e63
       }
     },
   ));
