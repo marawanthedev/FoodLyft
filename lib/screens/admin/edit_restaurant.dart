300deb723c6719b0bf6e69301b775233599efac9
Omar Hamed <Omarhamedchep93@gmail.com>
2021-02-13 23:31:03 +0800

    added editrestaurant screen

--- lib/screens/admin/edit_restaurant.dart
+++ lib/screens/admin/edit_restaurant.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class EditRestaurant extends StatefulWidget {
-  
-
   @override
   _EditRestaurantState createState() => _EditRestaurantState();
 }
@@ -11,20 +9,58 @@ class _EditRestaurantState extends State<EditRestaurant> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Edit Restaurant'),
+      appBar: AppBar(
+        title: Text('Edit Restaurant'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
-          child: ListView(children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Restaurant Name'),
-              textInputAction: TextInputAction.next,
-            ),
-          ],),
-        ),
+            child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Restaurant Name'),
+                textInputAction: TextInputAction.next,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Restaurant Category'),
+                textInputAction: TextInputAction.next,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(
+                      top: 9,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    child: Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'image'),
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        )),
       ),
-       
     );
   }
 }
